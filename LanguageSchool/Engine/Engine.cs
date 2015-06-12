efc7b8fb4e9dab1115cc5d867d9f7cde7ad1d20f
Miroslav Mirchev <mirchev1977@novihorizonti.eu>
2015-06-12 08:23:09 +0300

    Engine -> Initial Commit

---
+++ LanguageSchool/Engine/Engine.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LanguageSchool.Engine
+{
+    using Interfaces.Engine;
+
+    class Engine : IEngine
+    {
+        public void DispatchCommands(string commandStatement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteCreate(string commandStatement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteAdd(string commandStatement)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExecuteConduct(string commandStatement)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
